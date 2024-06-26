d9fbf82501defcd985ee62ce585828c74a6de869
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-06-26 16:11:54 +0530

    file handling and backend connection

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from functions.chatWithGemini import chatWithGemini
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 import os
+import mimetypes
 
 app = Flask(__name__)
 CORS(app)
@@ -24,15 +25,29 @@ def before_request():
 @app.route('/api/chat-gemini', methods=['POST'])
 def chatGemini():
     try:
-        data = request.get_json()
-        prompt_text = data['promptText']
-        prompt_img = data['promptImg']
-        response = chatWithGemini(prompt_text, prompt_img)
+        prompt_text = request.form.get('prompt_text')
+        prompt_file = request.files.get('prompt_file')
 
-        return jsonify({ 'AIReply' : response }), 200
+        allowed_extensions = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx', 'doc', 'xlsx', 'xls', 'mp4', 'avi', 'mov', 'mp3', 'wav', '.csv'}
+        
+        # Determine the mime type of the file
+        mime_type, _ = mimetypes.guess_type(prompt_file.filename)
+        # Save the file if it exists
+        file_path = None
+        if prompt_file:
+            file_path = os.path.join('uploads_temp', prompt_file.filename)
+            prompt_file.save(file_path)
+            prompt_file = file_path
+
+        response_text = chatWithGemini(prompt_text, prompt_file, mime_type)
+        return jsonify({"response": response_text})
     except Exception as e:
         print(f"Error occurred while chatting with Gemini:\n{e}")
         return jsonify({'errorMsg': "Something went wrong"}), 400
+    # finally:
+        # delete saved file if exists
+        # if prompt_file and os.path.exists(file_path):
+        #     os.remove(file_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
