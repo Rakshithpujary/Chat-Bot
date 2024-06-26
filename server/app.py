86c41d76a53104b6106c6d672ad2b65754f10a06
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-06-26 17:47:56 +0530

    file handling

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request, g
 import google.generativeai as genai
-from functions.chatWithGemini import chatWithGemini
+from functions.handleChat import handleImage, callGemini
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 import os
-import mimetypes
 
 app = Flask(__name__)
 CORS(app)
@@ -26,45 +25,30 @@ def before_request():
 def chatGemini():
     try:
         prompt_text = request.form.get('prompt_text')
-        prompt_file = request.files.get('prompt_file')
+        prompt_img = request.files.get('prompt_img')
 
-        allowed_extensions = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx', 'doc', 'xlsx', 'xls', 'mp4', 'avi', 'mov', 'mp3', 'wav', '.csv'}
-        
-        # Determine the mime type of the file
-        mime_type, _ = mimetypes.guess_type(prompt_file.filename)
-        # Save the file if it exists
-        file_path = None
-        if prompt_file:
-            file_path = os.path.join('uploads_temp', prompt_file.filename)
-            prompt_file.save(file_path)
-            prompt_file = file_path
+        allowed_extensions = {'png', 'jpg', 'jpeg'}
 
-        response_text = chatWithGemini(prompt_text, prompt_file, mime_type)
+        file_path = "dummy_path"
+        if prompt_img:
+            filename = prompt_img.filename
+            # Check if proper image extension
+            if '.' not in filename or filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
+                return jsonify({'errorMsg': 'File extension not allowed'}), 400
+
+            # retreive image in proper format so that gemini can handle
+            prompt_img, file_path = handleImage(prompt_img)
+
+        # Call Gemini
+        response_text = callGemini(prompt_text, prompt_img)
         return jsonify({"response": response_text})
     except Exception as e:
         print(f"Error occurred while chatting with Gemini:\n{e}")
         return jsonify({'errorMsg': "Something went wrong"}), 400
-    # finally:
+    finally:
         # delete saved file if exists
-        # if prompt_file and os.path.exists(file_path):
-        #     os.remove(file_path)
+        if prompt_img and os.path.exists(file_path):
+            os.remove(file_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-# import google.generativeai as genai
-# import PIL.Image
-# import os
-
-# genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-# img = PIL.Image.open('path/to/image.png')
-
-# model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-# response = model.generate_content(["What is in this photo?", img])
-# print(response.text)
-      
-
-# instead of generative model, use chat
---
+++ server/functions/handleChat.py
@@ -0,0 +1,60 @@
+from flask import g
+import textwrap
+import os
+import PIL.Image
+# import mimetypes
+# import fitz  # PyMuPDF
+
+# def saveFile(prompt_file):
+#     # Determine the mime type of the file
+#     mime_type, _ = mimetypes.guess_type(prompt_file.filename)
+#     # Save the file
+#     file_path = os.path.join('uploads_temp', prompt_file.filename)
+#     prompt_file.save(file_path)
+#     prompt_file = file_path
+
+#     return prompt_file, mime_type, file_path
+
+# def handleFile(prompt_file, mime_type):
+
+#     if mime_type and mime_type.startswith('image'):
+#         prompt_file = PIL.Image.open(prompt_file)
+#     elif mime_type.startswith('application/pdf'):
+#         prompt_file = fitz.open(prompt_file)
+#     elif mime_type.startswith('text/plain'):
+#         prompt_file = open_text(prompt_file)
+#     elif mime_type.startswith('audio'):
+#         prompt_file = open_audio(prompt_file)
+#     elif mime_type.startswith('video'):
+#         prompt_file = open_video(prompt_file)
+#     elif mime_type.startswith('application/vnd.openxmlformats-officedocument'):
+#         prompt_file = open_docx(prompt_file)
+
+#     return prompt_file
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return textwrap.indent(text, '> ', predicate=lambda _: True)
+
+def handleImage(prompt_img):
+    # Save the file
+    file_path = os.path.join('uploads_temp', prompt_img.filename)
+    prompt_img.save(file_path)
+    prompt_img = file_path
+
+    # get it ready for gemini ( for this saving is necassary )
+    prompt_img = PIL.Image.open(prompt_img)
+
+    return prompt_img, file_path
+
+def callGemini(prompt_text, prompt_img):
+
+    response = ""
+    if prompt_img is None:
+        response = g.chat.send_message(prompt_text) # if user did not send img, then send only text to gemini
+    else:
+        response = g.chat.send_message([prompt_text, prompt_img])  # Send both text and img to gemini
+
+    final_text = to_markdown(response.text)
+
+    return final_text
